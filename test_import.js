332635b05eb5e32e91dceb8992249da4ee0ac9bc
Marlon <marlon@example.com>
2026-09-06 13:58:29 -0300

    Aprimora leitor universal de Excel para aceitar todos os formatos de planilhas e abas

--- test_import.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Vamos baixar a biblioteca xlsx se necessário ou testar via CDN
-console.log('Iniciando análise do importSpreadsheet...');
